916143d3605e1a81f4b6be5a1309e818cc48ded3
Shaliefje <50870322+Shaliefje@users.noreply.github.com>
2025-04-01 14:16:26 +0200

    Delete patterns directory

--- patterns/contact-placeholder.php
+++
@@ -1,7 +0,0 @@
-<!-- wp:group {"className":"contact-form-placeholder","layout":{"type":"constrained"}} -->
-<!-- wp:image {"url":"https://via.placeholder.com/800x400","alt":"Contactformulier placeholder"} /-->
-
-<!-- wp:paragraph -->
-Het contactformulier wordt hier geplaatst via een plugin.
-<!-- /wp:paragraph -->
-<!-- /wp:group -->
